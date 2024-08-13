3ee9313f5ac134ded608b9fd1f78be4fd1fda342
bjoernbuth <bjoernbuth76@gmail.com>
2024-08-13 14:31:06 +0200

    temp. disable dumphelp_to_file command

--- clickhelper/dumphelp_to_file.py
+++ clickhelper/dumphelp_to_file.py
@@ -1,11 +1,7 @@
 import click
-import supercalc.click_options_abbrevs as click_abbrevs
-import supercalc.constants as constants
+import clickhelper.click_options_abbrevs as click_abbrevs
 import dataclasses
-
-import supercalc.main as main
 import re
-import pyperclip
 
 
 @dataclasses.dataclass
@@ -28,66 +24,61 @@ class HelpConfig:
     parent: str | None = None
 
 
-@click.command()
-@click_abbrevs.copy_to_clipboard_option
-@click_abbrevs.help_shorter_option
-@click_abbrevs.print_result_option
-@click.option(
-    "--number_sections/--no_number_sections",
-    "--ns/--nons",
-    is_flag=True,
-    default=False,
-    show_default=True,
-    help="Print result to console.",
-)
-def dumphelp_to_file(
-    copy_to_clipboard: bool = True,
-    print_result: bool = True,
-    number_sections: bool = False,
-):
-    """Dump the help to ../docs/dump_help.md."""
-    target_file = constants.PROJECT_ROOT / "doc" / "dump_help.md"
+# @click.command()
+# @click_abbrevs.copy_to_clipboard_option
+# @click_abbrevs.help_shorter_option
+# @click_abbrevs.print_result_option
+# @click.option(
+#     "--number_sections/--no_number_sections",
+#     "--ns/--nons",
+#     is_flag=True,
+#     default=False,
+#     show_default=True,
+#     help="Print result to console.",
+# )
+# def dumphelp_to_file(
+#     print_result: bool = True,
+#     number_sections: bool = False,
+# ):
+#     """Dump the help to ../docs/dump_help.md."""
+#     target_file = constants.PROJECT_ROOT / "doc" / "dump_help.md"
 
-    help_config = HelpConfig(
-        dotted_name="",
-        dotted_section="1",
-        is_root=True,
-        level=1,
-        number_sections=number_sections,
-        parent=None,
-    )
+#     help_config = HelpConfig(
+#         dotted_name="",
+#         dotted_section="1",
+#         is_root=True,
+#         level=1,
+#         number_sections=number_sections,
+#         parent=None,
+#     )
 
-    lines = recursive_help(cmd=main.suca, help_config=help_config)
+#     lines = recursive_help(cmd=main.suca, help_config=help_config)
 
-    # Remove the lines matching the pattern "Usage:" other stuff [OPTIONS] other stuff
-    pattern = r".*Usage.*\n*"
-    # remove all lines matching the pattern
+#     # Remove the lines matching the pattern "Usage:" other stuff [OPTIONS] other stuff
+#     pattern = r".*Usage.*\n*"
+#     # remove all lines matching the pattern
 
-    modified_lines = []
-    for line in lines:
-        line = re.sub(pattern, "", line)
-        modified_lines.append(line)
+#     modified_lines = []
+#     for line in lines:
+#         line = re.sub(pattern, "", line)
+#         modified_lines.append(line)
 
-    lines = modified_lines
+#     lines = modified_lines
 
-    res_text = "\n".join(lines)
+#     res_text = "\n".join(lines)
 
-    pattern_2 = r"```\n  "  # fix indent at the beginning of the code block
-    res_text = re.sub(pattern_2, "```\n", res_text)
+#     pattern_2 = r"```\n  "  # fix indent at the beginning of the code block
+#     res_text = re.sub(pattern_2, "```\n", res_text)
 
-    if print_result:
-        print(res_text)
-        # print("hi")
+#     if print_result:
+#         print(res_text)
+#         # print("hi")
 
-    with open(target_file, "w") as f:
-        f.write(res_text)
-        print("Dumped help to", target_file)
+#     with open(target_file, "w") as f:
+#         f.write(res_text)
+#         print("Dumped help to", target_file)
 
-    # re.sub(pattern, "", res_text)
-
-    if copy_to_clipboard:
-        pyperclip.copy(res_text)
-        print("Result copied to clipboard.")
+#     # re.sub(pattern, "", res_text)
 
 
 def recursive_help(
@@ -153,4 +144,5 @@ def recursive_help(
 
 
 if __name__ == "__main__":
-    dumphelp_to_file()
+    # dumphealp_to_file()
+    pass
